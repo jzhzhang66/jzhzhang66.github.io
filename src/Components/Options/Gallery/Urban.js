405a8a67e7a658545254953a68293b3da7c9046c
zhang.jud <zhang.jud@husky.neu.edu>
2020-04-20 19:38:15 -0400

    attempt to add video as background

--- src/Components/Options/Gallery/Urban.js
+++ src/Components/Options/Gallery/Urban.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import BurgerButton from '../../SideNav/BurgerButton';
+import '../../../App.css';
 
 class Urban extends React.Component {
     render() {
         return (
             <div>
-
+                <BurgerButton />
+                <div className="urban-container">
+                <iframe width="560" height="315" src="https://www.youtube.com/embed/WoZQ0ivvW7E?autoplay=1"
+                 frameborder="0" allow="accelerometer; autoplay; loop; encrypted-media; gyroscope; picture-in-picture"
+                  allowfullscreen></iframe>
+                    <h1>URBAN</h1>
+                </div>
             </div>
         )
     }
