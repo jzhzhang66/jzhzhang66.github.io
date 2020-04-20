e2abdc5e6eeb47c9ec4eb9fb710215fca97aea64
zhang.jud <zhang.jud@husky.neu.edu>
2020-04-20 17:54:28 -0400

    working drop down menu

--- src/Components/SideNav/SideNav.js
+++ src/Components/SideNav/SideNav.js
@@ -6,87 +6,15 @@ class SideNav extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            overallHovering: false,
-            headerHovering: false,
-            urbanHovering: false,
-            landscapeHovering: false,
-            portraitHovering: false
+            openDropdown: false
         }
-        this.setHeaderHovering = this.setHeaderHovering.bind(this);
-        this.setHeaderNotHovering = this.setHeaderNotHovering.bind(this);
-        this.setOptionHovering = this.setOptionHovering.bind(this);
-        this.setOptionNotHovering = this.setOptionNotHovering.bind(this);
-        this.setOverallHovering = this.setOverallHovering.bind(this);
+        this.toggleDropdown = this.toggleDropdown.bind(this);
     }
 
-    setHeaderHovering() {
+    toggleDropdown() {
         this.setState(() => {
             return {
-                headerHovering: true
-            }
-        })
-    }
-
-    setHeaderNotHovering() {
-        this.setState(() => {
-            return {
-                headerHovering: false
-            }
-        })
-    }
-
-   setOptionHovering = option => {
-        if (option === "Urban") {
-            this.setState(() => {
-                return {
-                    urbanHovering: true
-                }
-            })
-        }
-        if (option === "Landscape") {
-            this.setState(() => {
-                return {
-                    landscapeHovering: true
-                }
-            })
-        }
-        if (option === "Portrait") {
-            this.setState(() => {
-                return {
-                    portraitHovering: true
-                }
-            })
-        }
-    }
-
-    setOptionNotHovering = option => {
-        if (option === "Urban") {
-            this.setState(() => {
-                return {
-                    urbanHovering: false
-                }
-            })
-        }
-        if (option === "Landscape") {
-            this.setState(() => {
-                return {
-                    landscapeHovering: false
-                }
-            })
-        }
-        if (option === "Portrait") {
-            this.setState(() => {
-                return {
-                    portraitHovering: false
-                }
-            })
-        }
-    }
-
-    setOverallHovering() {
-        this.setState(() => {
-            return {
-                overallHovering: this.state.headerHovering || this.state.urbanHovering || this.state.landscapeHovering || this.state.portraitHovering ? true : false
+                openDropdown: !this.state.openDropdown
             }
         })
     }
@@ -94,19 +22,13 @@ class SideNav extends React.Component {
     dropDown() {
         return (
             <ul>
-                <Link onMouseOver={this.setOptionHovering("Urban"), this.setOverallHovering}
-                    onMouseOut={this.setOptionNotHovering("Urban"), this.setOverallHovering}
-                    to="/Gallery/Urban">
+                <Link to="/Gallery/Urban">
                     <li className="drop-down">Urban</li>
                 </Link>
-                <Link onMouseOver={this.setOptionHovering("Landscape"), this.setOverallHovering}
-                    onMouseOut={this.setOptionNotHovering("Landscape"), this.setOverallHovering}
-                    to="/Gallery/Landscape">
+                <Link to="/Gallery/Landscape">
                     <li className="drop-down">Landscape</li>
                 </Link>
-                <Link onMouseOver={this.setOptionHovering("Portrait"), this.setOverallHovering}
-                    onMouseOut={this.setOptionNotHovering("Portrait"), this.setOverallHovering}
-                    to="/Gallery/Portraits">
+                <Link to="/Gallery/Portraits">
                     <li className="drop-down">Portraits</li>
                 </Link>
             </ul>
@@ -122,10 +44,9 @@ class SideNav extends React.Component {
                 <Link to="/AboutMe">
                     <li>About Me</li>
                 </Link>
-                <Link onMouseOver={this.setHeaderHovering, this.setOverallHovering}
-                    onMouseOut={this.setHeaderNotHovering, this.setOverallHovering}>
+                <Link onClick={this.toggleDropdown}>
                     <li>Gallery</li>
-                    {this.state.overallHovering ? this.dropDown() : ""}
+                    {this.state.openDropdown ? this.dropDown() : ""}
                 </Link>
                 <Link to="/Contact">
                     <li>Contact</li>
