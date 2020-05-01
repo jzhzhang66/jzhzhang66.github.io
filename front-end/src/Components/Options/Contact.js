08d1ec0e5b907541f7577a7d38992fca73ebe75e
zhang.jud <zhang.jud@husky.neu.edu>
2020-05-01 14:26:54 -0400

    contact animations

--- front-end/src/Components/Options/Contact.js
+++ front-end/src/Components/Options/Contact.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import '../../App.css';
 import BurgerButton from '../SideNav/BurgerButton';
+import { motion } from "framer-motion";
 
 class Contact extends React.Component {
 
@@ -9,15 +10,16 @@ class Contact extends React.Component {
             <div>
                 <BurgerButton />
                 <div className="contact-div">
-                    <h1 className="contact-header">Contact</h1>
-                    <a>zhang.jud@husky.neu.edu</a>
-                    <br></br>
-                    <a>(201)-665-0678</a>
+                    <motion.h1 initial={{ x: -800, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease: "easeOut" }} className="contact-header">Contact</motion.h1>
+                    <motion.div initial={{ x: 800, opacity: 0 }} animate={{ x: 0, opacity: 1 }} transition={{ duration: 2, ease: "easeOut" }}>
+                        <a>zhang.jud@husky.neu.edu</a>
+                        <br></br>
+                        <a>(201)-665-0678</a>
+                    </motion.div>
                 </div>
             </div>
         )
     }
-
 }
 
 export default Contact;
